40f5fefbe2109b69829e2f9f33f923c4df784377
alexshapran-as <alexshapran.as@gmail.com>
2019-11-11 18:51:22 +0300

    RSA

--- src/main/scala/configurations/Conf.scala
+++ src/main/scala/configurations/Conf.scala
@@ -4,7 +4,6 @@ import com.typesafe.config.{Config, ConfigFactory}
 
 object Conf {
   val conf: Config = ConfigFactory.load("ATMKeeperBankApp_Configurations")
-  val confSecretKey: String = conf.getString("conf.bankservice.secretKey")
   val confApiServiceInterface: String = conf.getString("conf.apiservice.interface")
   val confApiServicePort: Int = conf.getInt("conf.apiservice.port")
 }
--- src/main/scala/services/ATMKeeperService.scala
+++ src/main/scala/services/ATMKeeperService.scala
@@ -1,18 +1,19 @@
 package services
 
+import java.security.PublicKey
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import com.typesafe.config.{Config, ConfigFactory}
-import authenticator.Authenticator._
 import akka.pattern._
 import akka.util.Timeout
+import cryptographer.RSA
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.concurrent.duration._
 
 object ATMKeeperService {
 
-  private case class Command(cmd: String, signature: String)
-  private case class Init(remote: ActorRef)
+  private case class Command(cmd: String)
+  private case class InitSecure(remote: ActorRef, publicKey: PublicKey)
 
   def remotingConfig(port: Int): Config = ConfigFactory.parseString(
     s"""
@@ -27,18 +28,20 @@ object ATMKeeperService {
             }
           }
         }
-    """) // "192.168.0.161"
+    """) // "10.50.1.61" BMSTU
 
   def remotingSystem(name: String, port: Int): ActorSystem = ActorSystem(name, remotingConfig(port))
 
   class BankService extends Actor {
 
     var remoteActorBBB: ActorRef = _
+    var remoteBBBPublicKey: PublicKey = _
 
     override def receive: Receive = {
 
-      case Init(remote) =>
+      case InitSecure(remote, publicKey) =>
         remoteActorBBB = remote
+        remoteBBBPublicKey = publicKey
         println(remoteActorBBB)
 
       case msg: String => println(msg)
@@ -46,7 +49,8 @@ object ATMKeeperService {
       case command: Command =>
         val realSender: ActorRef = sender
         implicit val timeout: Timeout = Timeout(10.seconds)
-        val response: Future[Either[String, (String, String)]] = (remoteActorBBB ? command).mapTo[Either[String, (String, String)]]
+        val response: Future[Either[String, (String, String)]] =
+          (remoteActorBBB ? RSA.encrypt(command.toString, remoteBBBPublicKey)).mapTo[Either[String, (String, String)]]
         response map {
           case Right(msgs) =>
             realSender ! Right(msgs)
@@ -62,7 +66,7 @@ object ATMKeeperService {
     val localActorBank: ActorRef = system.actorOf(Props[BankService], "bank")
     def sendCommandToBBB(cmd: String):  Future[Either[String, (String, String)]] = {
       implicit val timeout: Timeout = Timeout(10.seconds)
-      (localActorBank ? Command(cmd, generateHMAC(cmd))).mapTo[Either[String, (String, String)]]
+      (localActorBank ? Command(cmd)).mapTo[Either[String, (String, String)]]
     }
   }
 
