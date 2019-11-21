59534aed1f52004e23fd43f864c8639f1fdca341
alexshapran-as <alexshapran.as@gmail.com>
2019-11-22 00:23:02 +0300

    usb support

--- build.sbt
+++ build.sbt
@@ -8,5 +8,7 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.5.23",
   "com.typesafe.akka" %% "akka-remote" % "2.5.23",
   "com.typesafe.akka" %% "akka-http"   % "10.1.8",
-  "com.typesafe.akka" %% "akka-stream" % "2.5.23"
+  "com.typesafe.akka" %% "akka-stream" % "2.5.23",
+  "org.usb4java" % "usb4java" % "1.2.0",
+  "org.usb4java" % "usb4java-javax" % "1.2.0"
 )
--- src/main/scala/ATMKeeperBankApp.scala
+++ src/main/scala/ATMKeeperBankApp.scala
@@ -1,7 +1,12 @@
 import services.ApiService._
+import Usb4Scala._
+import javax.usb.UsbDevice
 
 object ATMKeeperBankApp {
   def main(args: Array[String]) {
+    val beagleBone: UsbDevice = Usb4Scala.getDevice(0x1d6b.toShort, 0x0104.toShort)
+    println(beagleBone)
+    Usb4Scala.sendBulkMessage(Usb4Scala.getDeviceInterface(beagleBone, 0), "Hello", 0)
     Routing().routingStart()
   }
 }
